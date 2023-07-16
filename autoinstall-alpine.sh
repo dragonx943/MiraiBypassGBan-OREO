e60b39d674931c91918cfefc114342b889cb2273
Draken <131387159+dragonx943@users.noreply.github.com>
2023-07-16 13:48:05 +0700

    Update

--- autoinstall-alpine.sh
+++ autoinstall-alpine.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "N: Update packages and install some important packages into your machine..."
-apk update && apk add sudo nano git python3 gcc g++ make wget pkgconfig
+apk update && apk add sudo nano git gcc g++ make wget pkgconfig
 
 echo "W: Install python3 as default..."
 apk add --update --no-cache python3 && ln -sf python3 /usr/bin/python 
@@ -24,10 +24,9 @@ if [ ! -f $tarball ]; then
                 esac
                 curl -fLO https://github.com/oznu/alpine-node/releases/download/16.15.1/node-v16.15.1-linux-${archurl}-alpine.tar.gz
                 echo "W: Installing nodejs & npm..."
-                tar -xzf node-v16.15.1-linux-{your arch}-alpine.tar.gz -C /usr --strip-components=1 --no-same-owner
+                tar -xzf node-v16.15.1-linux-${archurl}-alpine.tar.gz -C /usr --strip-components=1 --no-same-owner
                 echo "W: Cleaning up..."
                 rm -rf node-v16.15.1-linux-*-alpine.tar.gz
-                echo "W: Successful installation."
+                echo "N: Successful installation."
 fi
-echo "W: Not detected your Architecture, stoping executive command..."
-echo "E: Install packages failed !!!"
+echo "W: Exiting..."
