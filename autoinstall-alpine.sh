8d0f2e917bdafb420b5a8c793ee50417f98e4162
Draken <131387159+dragonx943@users.noreply.github.com>
2023-07-29 23:28:30 +0700

    Fix bug, bugn't ?

--- autoinstall-alpine.sh
+++ autoinstall-alpine.sh
@@ -12,32 +12,21 @@ echo "Bắt đầu cài đặt những gói dữ liệu cần thiết..."
 apk add --no-cache libgcc libstdc++ build-base cairo-dev jpeg-dev pango-dev freetype-dev giflib-dev
 clear
 
-# Detect your architecture into value to download and install nodejs 16:
-tarball="node-v16.15.1-linux-*-alpine.tar.gz"
-if [ ! -f $tarball ]; then
-                echo "Đang tải Nodejs v16, có thể mất một lúc dựa vào đường truyền kết nối mạng của bạn..."
-                case `uname -m` in
-                i386)
-                        archurl="x86" ;;
-                i686)
-                        archurl="x86" ;;
-                x86)
-                        archurl="x86" ;;
-                aarch64)
-                        archurl="arm64" ;;
-                arm)
-                        archurl="arm" ;;
-                amd64)
-                        archurl="x86_64" ;;
-                x86_64)
-                        archurl="x86_64" ;;
-                esac
-                curl -fLO https://github.com/dragonx943/alpine-node/releases/download/v16.15.1/node-v16.15.1-linux-${archurl}-alpine.tar.gz
-                echo "W: Đang cài đặt nodejs & npm..."
-                tar -xzf node-v16.15.1-linux-${archurl}-alpine.tar.gz -C /usr --strip-components=1 --no-same-owner
-                echo "W: Đang dọn dẹp..."
-                rm -rf node-v16.15.1-linux-*-alpine.tar.gz
-fi
+# Download and install nodejs 16:
+echo "N: Bắt đầu cài đặt nodejs sau 10s..."
+sleep 10
+PS3="Chọn số tương ứng với phiên bản nodejs bạn muốn cài đặt: "
+select lng in v16 v18
+do
+    case $lng in
+        "v16")
+           ash <(curl -Ls https://raw.githubusercontent.com/dragonx943/alpine-node/main/setup-node16.sh);;
+        "v18")
+           ash <(curl -Ls https://raw.githubusercontent.com/dragonx943/alpine-node/main/setup-nodejs18.sh);;
+        *)
+           echo "E: Nhập sai số tương ứng với phiên bản nodejs, vui lòng thử lại !!!";;
+    esac
+done
 clear
 echo "Đã thực thi cài đặt các gói chương trình cơ bản & cần thiết! Nếu xảy ra lỗi, vui lòng chạy lại trình cài đặt này!"
 sleep 5
