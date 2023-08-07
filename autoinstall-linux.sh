74050a67950db1fe0ca75092948420ab5cc185e0
Draken <131387159+dragonx943@users.noreply.github.com>
2023-08-07 18:49:23 +0000

    Update autoinstall-linux.sh

--- autoinstall-linux.sh
+++ autoinstall-linux.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 ## This commands for linux have "apt" like Ubuntu or Debian or something else...NEED ROOT
 
 ## Some crazy note
@@ -11,17 +11,16 @@ sleep 5
 clear
 
 ## Install some important packages from repo archive
-echo "Bắt đầu cập nhật các gói dữ liệu từ máy chủ của hệ điều hành..."
+echo "N: Bắt đầu cập nhật các gói dữ liệu từ máy chủ của hệ điều hành..."
 apt update && apt upgrade -y
 clear
-echo "Bắt đầu cài đặt các gói cần thiết cho hệ điều hành..."
+echo "W: Bắt đầu cài đặt các gói cần thiết cho hệ điều hành..."
 apt install sudo git nano wget python3 python-is-python3 gcc g++ make build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev -y --auto-remove
 clear
 
 ## Install nodejs 16
-echo "Bắt đầu cài đặt nodejs v16...Nếu trình cài đặt bị đứng ở đây, vui lòng khởi động lại trình cài đặt gốc!"
+echo "N: Bắt đầu cài đặt nodejs v16...Nếu trình cài đặt bị đứng ở đây, vui lòng khởi động lại trình cài đặt gốc!"
 cd ~
 curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
-clear
 sudo apt install nodejs -y
 clear
